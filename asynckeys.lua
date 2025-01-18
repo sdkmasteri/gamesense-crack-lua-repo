bce1600000eeb348aaf5570fde2208cf891a8999
sdkmasteri <ilya1234512346@gmail.com>
2025-01-18 02:25:26 +0200

    download icons

--- asynckeys.lua
+++ asynckeys.lua
@@ -1,4 +1,38 @@
---coded by sakenzo
+--credist: @dave8x3
+local ffi = require("ffi")
+local http = require("gamesense/http")
+local filesystem_interface = ffi.cast(ffi.typeof("void***"), client.create_interface("filesystem_stdio.dll", "VFileSystem017"))
+local filesystem_create_directories = ffi.cast("void (__thiscall*)(void*, const char*, const char*)", filesystem_interface[0][22])
+local filesystem_find = ffi.cast("const char* (__thiscall*)(void*, const char*, int*)", filesystem_interface[0][32])
+local function create_directories(file, path_id)
+    filesystem_create_directories(filesystem_interface, file, path_id)
+end
+local exists = function(file)
+    local int_ptr = ffi.new("int[1]")
+    local res = filesystem_find(filesystem_interface, file, int_ptr)
+    if res == ffi.NULL then
+        return nil
+    end
+
+    return int_ptr, ffi.string(res)
+end
+local function download_icon(name)
+    local file_path = ("csgo/keys/%s.png"):format(name)
+    http.get(("https://raw.githubusercontent.com/sdkmasteri/gamesense-crack-lua-repo/refs/heads/main/keys/%s.png"):format(name), function(status, response)
+        if not status then
+            return error(string.format("Cant load %s icon", name))
+        end
+
+        writefile(file_path, response.body)
+    end)
+end
+if not exists("keys") then create_directories("keys", "keys") end
+for _, name in pairs({"w", "a", "s", "d", "w_active", "a_active", "s_active", "d_active"}) do
+    if not readfile(string.format("csgo/keys/%s.png", name)) then
+        download_icon(name)
+    end
+end
+--credits: @sdkamsteri
 local enabler = ui.new_checkbox("Visuals", "Other ESP", "Asynckeys")
 math.lerp = function(start, end_pos, time) if start == end_pos then return end_pos end local frametime = globals.frametime() * (1/globals.frametime()); time = time * frametime; local val = start + (end_pos - start) * time; if(math.abs(val - end_pos) < 0.01) then return end_pos end return val end
 
@@ -109,14 +143,7 @@ client.set_event_callback("paint", function()
     firstclick = not oldclicked and clicked
 end)
 --@region dragble end
-local w_key = renderer.load_png(readfile("csgo/keys/w.png"), 52, 52)
-local w_ac = renderer.load_png(readfile("csgo/keys/w_active.png"), 52, 52)
-local a_key = renderer.load_png(readfile("csgo/keys/a.png"), 52, 52)
-local a_ac = renderer.load_png(readfile("csgo/keys/a_active.png"), 52, 52)
-local s_key = renderer.load_png(readfile("csgo/keys/s.png"), 52, 52)
-local s_ac = renderer.load_png(readfile("csgo/keys/s_active.png"), 52, 52)
-local d_key = renderer.load_png(readfile("csgo/keys/d.png"), 52, 52)
-local d_ac = renderer.load_png(readfile("csgo/keys/d_active.png"), 52, 52)
+
 
 local alph = {
     ["W"] = 0,
@@ -125,6 +152,14 @@ local alph = {
     ["D"] = 0
 }
 client.set_event_callback("paint",  function()
+    local w_key = renderer.load_png(readfile("csgo/keys/w.png"), 52, 52)
+    local w_ac = renderer.load_png(readfile("csgo/keys/w_active.png"), 52, 52)
+    local a_key = renderer.load_png(readfile("csgo/keys/a.png"), 52, 52)
+    local a_ac = renderer.load_png(readfile("csgo/keys/a_active.png"), 52, 52)
+    local s_key = renderer.load_png(readfile("csgo/keys/s.png"), 52, 52)
+    local s_ac = renderer.load_png(readfile("csgo/keys/s_active.png"), 52, 52)
+    local d_key = renderer.load_png(readfile("csgo/keys/d.png"), 52, 52)
+    local d_ac = renderer.load_png(readfile("csgo/keys/d_active.png"), 52, 52)
     if not ui.get(enabler) then return end
     local x, y = client.screen_size()
     local w, h = 100, 100
