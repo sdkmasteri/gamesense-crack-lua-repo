b217f57f9369e53431ece18d89d056fa4196895e
sdkmasteri <ilya1234512346@gmail.com>
2025-06-15 10:46:13 +0300

    changed some gownocode

--- Hide tabs.lua
+++ Hide tabs.lua
@@ -6,32 +6,30 @@ local base = ffi.cast("int*", 0x43479A04)
 local rbase = ffi.cast("int*", 0x43479A00) -- rage tab class 4 bytes bellow other tabs base
 
 local tabs = {}
-local vals = {}
 --do not add lua tab, cuz you will eventually brick menu))
 local tochange = {"Rage", "AA", "Legit", "Visuals", "Misc", "Skins", "Plist", "Config"}
 
 tabs[0] = ffi.cast("char*", ffi.cast("int*", rbase[0])[0] + 0x15)
-vals[0] = tabs[0][0] == 0x01 and true or false
 for i=1, #tochange do
     local tabclass = ffi.cast("int*", base[0] - (0x20 - 0x4*(i-1)))
     tabs[i] = ffi.cast("char*", tabclass[0] + 0x15)
-    vals[i] = tabs[i][0] == 0x01 and true or false
 end
 
 local disablebox    = ui.new_listbox("LUA", "A", "Disable tabs", tochange) 
 local checkbox      = ui.new_checkbox("LUA", "A", "Active")
 
-ui.set_callback(disablebox, function(id)
-    local choosen = ui.get(id)
-    ui.set(checkbox, vals[choosen])
-end)
+local function updcheck()
+    ui.set(checkbox, tabs[ui.get(disablebox)][0] == 0x01 and true or false)
+end
+
+updcheck()
+
+ui.set_callback(disablebox, updcheck)
 
 local function changevisibl(index, bool)
     tabs[index][0] = bool and 0x01 or 0x00
-    vals[index] = bool
 end
 
 ui.set_callback(checkbox, function(id)
-    local current = ui.get(disablebox)
-    changevisibl(current, ui.get(id))
+    changevisibl(ui.get(disablebox), ui.get(id))
 end)
